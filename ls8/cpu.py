8e5991f6a1560182c6c2ef7de2809c84ea3e39ab
nomadic4life <orbison.dennis@gmail.com>
2019-08-21 21:45:43 -0700

    completed load and able to run multi.ls8 file

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -86,7 +86,7 @@ class CPU:
             # 'ST': self.exec_st,
             # 'ADD': self.exec_add,
             # 'SUB': self.exec_sub,
-            # 'MUL': self.exec_mul,
+            'MUL': self.exec_mul,
             # 'DIV': self.exec_div,
             # 'MOD': self.exec_mod,
             # 'CMP': self.exec_cmp,
@@ -109,26 +109,45 @@ class CPU:
         a = operand["operand_a"]
         print(self.reg[a])
 
+    def exec_mul(self, operand):
+        a = operand["operand_a"]
+        b = operand["operand_b"]
+        self.alu('MUL', a, b)
+
+    # def load(self):
+    #     """Load a program into memory."""
+
+    #     address = 0
+
+    #     # For now, we've just hardcoded a program:
+
+    #     program = [
+    #         # From print8.ls8
+    #         0b10000010,  # LDI R0,8
+    #         0b00000000,
+    #         0b00001000,
+    #         0b01000111,  # PRN R0
+    #         0b00000000,
+    #         0b00000001,  # HLT
+    #     ]
+
+    #     for instruction in program:
+    #         self.ram[address] = instruction
+    #         address += 1
+
     def load(self):
         """Load a program into memory."""
+        if len(sys.argv) != 2:
+            print("Error: wrong number of arguments")
+            sys.exit()
 
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(sys.argv[1]) as f:
+            for line in f:
+                tmp = line.split('#', 1)[0].strip()
+                if tmp:
+                    self.ram_write(int(tmp, 2), address)
+                    address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -136,6 +155,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         # elif op == "SUB": etc
+        elif op == 'MUL':
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
